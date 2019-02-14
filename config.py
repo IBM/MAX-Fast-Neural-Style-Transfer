b0c83fce28d8eca4a2c6501cb10a8fd8433c7e69
Brendan Dwyer <brendan.dwyer@ibm.com>
2019-02-14 15:17:35 -0800

    set API_TITLE and update tests

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ RESTPLUS_MASK_SWAGGER = False
 SWAGGER_UI_DOC_EXPANSION = 'none'
 
 # API metadata
-API_TITLE = 'Fast Neural Style Transfer'  # TODO decide on the name for this
+API_TITLE = 'MAX Fast Neural Style Transfer'
 API_DESC = 'Generate a new image that mixes the content of a source image with the style of another image.'
 API_VERSION = '0.1'
