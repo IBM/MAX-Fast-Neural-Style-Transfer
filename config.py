a9f35b82c939bde33ee8b4eded161ee7c1cd12b1
Nick Pentreath <nickp@za.ibm.com>
2019-07-05 12:10:41 +0200

    Bump version in prep for next release

--- config.py
+++ config.py
@@ -26,4 +26,4 @@ SWAGGER_UI_DOC_EXPANSION = 'none'
 # API metadata
 API_TITLE = 'MAX Fast Neural Style Transfer'
 API_DESC = 'Generate a new image that mixes the content of a source image with the style of another image.'
-API_VERSION = '1.1.0'
+API_VERSION = '1.2.0'
